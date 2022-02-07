6ed3c550e693e9f834044717ae402ec8c72a9eff
fernando gabriel <moreragabriel2006@gmail.com>
2022-02-07 11:41:24 +0530

    poor arnav needs sleep i added few more files

---
+++ datafile.py
@@ -0,0 +1,133 @@
+import discord 
+from discord.ext import commands
+
+
+
+Gifdata=[
+    'https://c.tenor.com/iozuy0_sdygAAAAC/what-the-dog-doing-dog.gif',
+    'https://c.tenor.com/QRtdxdOR6ukAAAAM/i-dont-remember-asking.gif',
+    'https://c.tenor.com/p98d_YNK3K8AAAAC/gordon-ramsay-idiot-sandwich.gif',
+    'https://c.tenor.com/4fH8zSIuSvcAAAAM/cristiano-ronaldo-soccer.gif',
+    'https://c.tenor.com/4tBl5ArWwAgAAAAd/neymar-roll-neymar.gif',
+    'https://c.tenor.com/GKc7JcrlQ-sAAAAM/ninja-fortnite.gif',
+    'https://c.tenor.com/4Y6tb8pX7CwAAAAM/mongraal.gif',
+    'https://c.tenor.com/0-iYZUWms_8AAAAC/minecraft-ghast.gif',
+    'https://c.tenor.com/ATYLM3TJsykAAAAC/ahhhh-its-wednesday-my-dudes.gif',
+    'https://c.tenor.com/_seZCixIdGYAAAAC/travis-scott-falling.gif',
+    'https://c.tenor.com/LoQoNueBMw0AAAAd/travis-scott-travis-scott-apology.gif',
+    'https://c.tenor.com/OsnAgZjC-kQAAAAM/underwater-hello-im-under-water.gif',
+    'https://c.tenor.com/XTyMxI_cHTkAAAAd/rick-roll.gif',
+    'https://c.tenor.com/LIJVfqGBMVUAAAAd/rickroll-meme.gif',
+    'https://c.tenor.com/TTfEcL3R8ToAAAAM/monkeys.gif',
+    'https://c.tenor.com/FD9HOQNGbNsAAAAd/uno-reverse.gif',
+              ]
+
+#memes 
+Imagedata=[
+    'https://img.ifunny.co/images/b6b9fefe99aae9ec276fab9f541af0ad7165f0556c3dff638113acc2e4cd05a6_1.jpg',
+    'https://i.kym-cdn.com/entries/icons/original/000/036/684/bananacover.jpg',
+    'https://i.pinimg.com/564x/c3/57/65/c357655f71dd60653408727b86b6a313.jpg',
+    'https://cdn.i-scmp.com/sites/default/files/styles/1200x800/public/d8/images/canvas/2021/10/30/85f2cb5f-44f8-4f2f-a813-63e657e11acc_5065cac7.jpg?itok=gY-K9HdU&v=1635566576',
+    'https://cdn.discordapp.com/attachments/920531846113136730/939742543288074270/644tk0.jpg'
+    ]
+
+Imagedata2=[
+    'https://cdn.discordapp.com/attachments/849904393906421770/938776994022297600/63si81.jpg',
+    'https://cdn.discordapp.com/attachments/740866748546416672/938778414012317726/63sirw.jpg',
+    'https://cdn.discordapp.com/attachments/742948348561981461/938781599300997170/63sk8c.jpg',
+    'https://cdn.discordapp.com/attachments/740866748546416672/939369379081097216/6418gr.jpg',
+    'https://cdn.discordapp.com/attachments/849904393906421770/938819822446342144/a8768cd8df2c0aa8b42729f0512a4eb9.jpg',
+    'https://cdn.discordapp.com/attachments/849904393906421770/939025671030710282/New_Project.png',
+]
+
+
+
+rating=["You are 1% Cool",
+        "You arent cool",
+        "You are not cool",
+        "You are 24% Depressed",
+        "You are 100% not cool",
+        "I am sorry who are you?",
+        "Your Social credit score is -1000000 Not bing chilling",
+        "I am sorry but i dont know who you are",
+        "You are so Ugly",
+        "Stop being so annoying",
+        "Taiwan is a country so no you have -Infinite social credit score",
+        "If you keep raging in fortnite i cant rate you cause you have rag issues "
+    ]
+
+ballresponses=["It is certain.",
+                "It is decidedly so.",
+                "Without a doubt.",
+                "Yes - definitely.",
+                "You may rely on it.",
+                "As I see it, yes.",
+                "Most likely.",
+                "Outlook good.",
+                "Yes.",
+                "Signs point to yes.",
+                "Reply hazy, try again.",
+                "Ask again later.",
+                "Better not tell you now.",
+                "Cannot predict now.",
+                "Concentrate and ask again.",
+                "Don't count on it.",
+                "My reply is no.",
+                "My sources say no.",
+                "Outlook not so good.",
+                "Very doubtful.",
+                "Touch Grass"]
+
+#bot description 
+description=('Hello there new user.\nI am phoenix classroom.'
+ '\n I am a bot developed for this Grade 9 Discord Server of Gems Modern Academy Kochi.'
+ '\n If you are a new member please select your roles in reaction roles.'
+ '\n Here are the Commands which can you use'
+ )
+#bot embed image url 
+botimage='https://lh3.googleusercontent.com/xuwBaIy4dCq95kX2cEMq_6GnB6Ed2B3imfZW4UQvUlo2MGMy2QiCAfm7r6BNj2jAeA'
+#bot status
+bs=['Never gonna',
+ 'Gems.ae',
+ 'Fortnite',
+ 'Halo Infinite',
+ 'Assignments',
+ '-help']
+#bot version
+botver=('v8.90')
+
+#ban or kick embed
+banembed=['https://c.tenor.com/Db1OikoTLjoAAAAM/salt-bae-ban.gif']
+
+color1=[ 
+ discord.Color.blue(),
+ discord.Color.red(),
+ discord.Color.gold(),
+ discord.Color.green()
+]
+
+#lyrics 
+lyrics=[
+    "We're no strangers to love",
+"You know the rules and so do I",
+"A full commitment's what I'm thinking of",
+"You wouldn't get this from any other bot",
+"I just wanna tell you how I'm feeling",
+"Gotta make you understand",
+"Never gonna give you up",
+"Never gonna let you down",
+"Never gonna run around and desert you",
+"Never gonna make you cry",
+"Never gonna say goodbye",
+"Never gonna tell a lie and hurt you",
+"We've known each other for so long",
+"Your heart's been aching but you're too shy to say it",
+"Inside we both know what's been going on",
+"We know the game and we're gonna play it",
+"And if you ask me how I'm feeling",
+"Don't tell me you're too blind to see",]
+
+url=['https://c.tenor.com/eBltHZ96be4AAAAd/dans-opgeven.gif',
+'https://c.tenor.com/uzmFUkzbrxkAAAAM/rick-astley-rules.gif',
+'https://c.tenor.com/Tdqws1Nmha4AAAAd/rick-roll-deal-with-it.gif',
+'https://c.tenor.com/kI-z88DBnu8AAAAC/rick-roll-my-beloved.gif']
